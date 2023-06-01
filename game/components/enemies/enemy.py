130e01fe80adc301dcf08a429bdce739079a3c30
YhorDul <Yhordan.Choque@fundacion-jala.org>
2023-05-31 21:37:25 -0400

    Third-Class

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -3,6 +3,7 @@ import pygame
 
 from pygame.sprite import Sprite
 from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH, ENEMY_2, ENEMY_3, ENEMY_4, ENEMY_MASTER, OVNI
+from game.components.bullets.bullet import Bullet
 
 class Enemy(Sprite):
 
@@ -14,6 +15,7 @@ class Enemy(Sprite):
     SHIP_WIDTH = 40
     SHIP_HEIGHT = 60
     TYPE_ENEMY = {0: ENEMY_4, 1: ENEMY_3, 2: ENEMY_1, 3: ENEMY_2}
+    
 
     def __init__(self):
         self.image = self.TYPE_ENEMY[random.randint(0, 3)]
@@ -26,9 +28,12 @@ class Enemy(Sprite):
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
+        self.type = 'enemy'
+        self.shooting_time = random.randint(30, 50)
 
-    def update(self, ships):
+    def update(self, ships, game):
         self.rect.y += self.speed_y
+        self.shoot(game.bullet_manager)
 
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x
@@ -50,3 +55,11 @@ class Enemy(Sprite):
         elif(self.index >= self.move_x_for and self.movement_x =='left') or (self.rect.x <= 10):
             self.movement_x = 'rigth'
             self.index = 0
+
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(30, 50)
+
